1c0d6c3be75a87dc8fb9c6da082e5dea26ba0f75
Durgasri <durgasri.240081@aids.ritchennai.edu.in>
2025-08-12 18:36:20 +0530

    Sum.java

---
+++ Sum.java
@@ -0,0 +1,75 @@
+//Durgasri S
+//2117240070081
+program title:Sum of all numers from 1 to 100
+Tc:1
+  public class Sum {
+	public static void main(String[] args)
+	{
+		int sum=0;
+		for(int i=1;i<=100;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.print("the sum of all numbers from 1 to 100 is"+sum);
+	}
+
+}
+output:the sum of all numbers from 1 to 100 is5050
+Tc:2
+  public class Sum {
+	public static void main(String[] args)
+	{
+		int sum=0;
+		for(int i=1;i<=50;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.print("the sum of all numbers from 1 to 50 is"+sum);
+	}
+
+}
+output:the sum of all numbers from 1 to 50 is1275
+  Tc:3
+  public class Sum {
+	public static void main(String[] args)
+	{
+		int sum=0;
+		for(int i=1;i<=10;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.print("the sum of all numbers from 1 to 10 is"+sum);
+	}
+
+}
+output:the sum of all numbers from 1 to 10 is55
+Tc:4
+  public class Sum {
+	public static void main(String[] args)
+	{
+		int sum=0;
+		for(int i=50;i<=100;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.print("the sum of all numbers from 50 to 100 is"+sum);
+	}
+
+}
+output:the sum of all numbers from 50 to 100 is3825
+Tc:5
+  public class Sum {
+	public static void main(String[] args)
+	{
+		int sum=0;
+		for(int i=1;i<=1;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.print("the sum of all numbers from 1 to 1 is"+sum);
+	}
+
+}
+output:the sum of all numbers from 1 to 1 is1
+		
+
